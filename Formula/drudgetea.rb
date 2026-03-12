b7d31499c02597a144d2aceadd23c69f43062524
mikegetz <getzmd02@gmail.com>
2026-03-11 17:37:01 -0700

    v1.2.11

--- Formula/drudgetea.rb
+++ Formula/drudgetea.rb
@@ -1,30 +1,30 @@
 class Drudgetea < Formula
   desc "Bubble Tea TUI for Drudge Report"
   homepage "https://github.com/mikegetz/drudgetea"
-  version "1.2.9"
+  version "1.2.11"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/mikegetz/drudgetea/releases/download/v1.2.9/drudgetea-v1.2.9-darwin-amd64"
-      sha256 "ddf7de54a45c8557fe21a7c59da8e214af0dbc1c9dfc5e5f4a4b2e7492f754d8"
+      url "https://github.com/mikegetz/drudgetea/releases/download/v1.2.11/drudgetea-v1.2.11-darwin-amd64"
+      sha256 "02e3eae8327b79e01a5ba3ea657308dc3d723135059769b72a03e988ed9a7dd8"
     end
 
     on_arm do
-      url "https://github.com/mikegetz/drudgetea/releases/download/v1.2.9/drudgetea-v1.2.9-darwin-arm64"
-      sha256 "acdc763ac8e1df45e768239130ffdc250459081cd974ffb7b0604d6a5cfeeeeb"
+      url "https://github.com/mikegetz/drudgetea/releases/download/v1.2.11/drudgetea-v1.2.11-darwin-arm64"
+      sha256 "8bb3327305b99d85621fb6146438d2b634d322654c500a083ad3a0cdb6e2cb51"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/mikegetz/drudgetea/releases/download/v1.2.9/drudgetea-v1.2.9-linux-amd64"
-      sha256 "7147d40b0a80440ab6d588403f1b57bec3c1df82fb7fe39ddaabc4ff19bf7666"
+      url "https://github.com/mikegetz/drudgetea/releases/download/v1.2.11/drudgetea-v1.2.11-linux-amd64"
+      sha256 "155244541f64e04db49be92e292afba234837284d90d4df4a7baa64d95209dc2"
     end
 
     on_arm do
-      url "https://github.com/mikegetz/drudgetea/releases/download/v1.2.9/drudgetea-v1.2.9-linux-arm64"
-      sha256 "60e37042632705a3229258194319ba5faa7fe002dcc235ae357791de43591f7e"
+      url "https://github.com/mikegetz/drudgetea/releases/download/v1.2.11/drudgetea-v1.2.11-linux-arm64"
+      sha256 "a4ad2863675b772528b4fb12613f9a57e77cc2d9b5f820e2edd0177282b66159"
     end
   end
 
